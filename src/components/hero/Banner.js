a020aa1cfdb6318bbc784de490e08344c1a56cd2
abdul-sam <abdul@gininow.com>
2024-03-17 02:47:08 +0500

    changes for class to className in banner section

--- src/components/hero/Banner.js
+++ src/components/hero/Banner.js
@@ -8,9 +8,9 @@ import { Styles } from "../../styles";
 const Banner = () => {
   return (
     <div className="bg-gray-50 pt-20">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-40 lg:px-12">
-        <div class="flex flex-col items-center justify-between gap-5 pt-6 lg:flex-row">
-          <div class="flex-1 w-full lg:-mt-6">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-40 lg:px-12">
+        <div className="flex flex-col items-center justify-between gap-5 pt-6 lg:flex-row">
+          <div className="flex-1 w-full lg:-mt-6">
             <Typography
               variant="h1"
               className="font-mono flex justify-center items-center text-6xl mb-10"
@@ -30,29 +30,29 @@ const Banner = () => {
             >
               PakPets is perfect place for your dream pets. Find your pets, shops & pet foods.
             </Typography>
-            <div class="mt-8">
+            <div className="mt-8">
               <form method="post">
-                <div class="flex flex-col w-full gap-4 md:gap-0 md:flex-row">
-                  <div class="relative flex-1 ">
-                    <div class={`${Styles.icon.wrapper} pl-5`}>
+                <div className="flex flex-col w-full gap-4 md:gap-0 md:flex-row">
+                  <div className="relative flex-1 ">
+                    <div className={`${Styles.icon.wrapper} pl-5`}>
                       <MagnifyingGlassIcon className={`${Styles.icon.default}`}/>
                     </div>
                     <input
                       type="text"
                       name="keyword"
                       placeholder="Pet Keyword"
-                      class={`${Styles.input.default} md:rounded-l-full`}
+                      className={`${Styles.input.default} md:rounded-l-full`}
                     />
                   </div>
-                  <div class="relative flex-1 ">
-                    <div class={`${Styles.icon.wrapper} pl-3.5`}>
+                  <div className="relative flex-1 ">
+                    <div className={`${Styles.icon.wrapper} pl-3.5`}>
                       <MapPinIcon className={`${Styles.icon.default}`}/>
                     </div>
                     <input
                       type="text"
                       name="keyword"
                       placeholder="Location"
-                      class={`${Styles.input.default} md:border-l-transparent`}
+                      className={`${Styles.input.default} md:border-l-transparent`}
                     />
                   </div>
                   <Button
@@ -65,7 +65,7 @@ const Banner = () => {
                 </div>
               </form>
             </div>
-            <div class="flex flex-wrap gap-3 mt-8 text-center justify-center items-center">
+            <div className="flex flex-wrap gap-3 mt-8 text-center justify-center items-center">
               <Typography
                 variant="p"
                 className="font-bold"
