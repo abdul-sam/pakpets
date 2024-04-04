b5d765a1169f8c4d7e56de61c5096b6c3111051a
abdul-sam <abdulsamad.3939@gmail.com>
2024-04-04 21:14:02 +0500

    Added Rating logic and changes for verfied for shop and breeders

--- src/constants/breeders.js
+++ src/constants/breeders.js
@@ -7,36 +7,40 @@ export const Breeders = [
     member_since: '11 months',
     address: 'Lahore, Punjab',
     phone: "+92 311 0354678",
+    rating: 4,
     verified: true
   },
   {
     id: 2,
-    name: "Khawar",
+    name: "Khawar Aslam",
     about: "Hashtag Blue Bottle Williamsburg Kickstarter gluten-free mustache jean shorts.",
     profile_image: "https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
     member_since: '8 years',
-    address: 'Lahore, Punjab',
+    address: 'Multan, Punjab',
     phone: "+92 311 0354678",
+    rating: 4.6,
     verified: false
   },
   {
     id: 3,
-    name: "Ahsan",
+    name: "Ahsan Hassan",
     about: "Hashtag Blue Bottle Williamsburg Kickstarter gluten-free mustache jean shorts.",
     profile_image: "https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
     member_since: '3 years',
-    address: 'Lahore, Punjab',
+    address: 'Rawalpindi, Punjab',
     phone: "+92 311 0354678",
+    rating: 3.9,
     verified: true
   },
   {
     id: 4,
-    name: "M Faizan",
+    name: "Faizan Haider",
     about: "Hashtag Blue Bottle Williamsburg Kickstarter gluten-free mustache jean shorts.",
     profile_image: "https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
     member_since: '12 years',
     address: 'Lahore, Punjab',
     phone: "+92 311 0354678",
+    rating: 3.5,
     verified: true
   },
   
